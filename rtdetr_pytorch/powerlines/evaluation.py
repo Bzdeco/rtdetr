0b772635aa730daaa8a020a8e4cc76137b4f87b0
Jakub Gwizdała <jakub.gwizdala@gmail.com>
2024-07-04 19:41:28 +0200

    Update bin thresholds

--- rtdetr_pytorch/powerlines/evaluation.py
+++ rtdetr_pytorch/powerlines/evaluation.py
@@ -20,7 +20,7 @@ def mean_average_precision():
 def ccq(downsampling_factor: int, mask_exclusion_zones: bool, tolerance_region: float = 1.42):
     return CCQMetric(
         # standard + default + NEVBW 2 cells distance, rescaled to full range (as distance mask are generated and not clamped)
-        bin_thresholds=np.asarray([0.5, 1.5, 2.5]),
+        bin_thresholds=np.asarray([0.5, 1.28, 1.5]),
         tolerance_region=tolerance_region,
         downsampling_factor=downsampling_factor,
         mask_exclusion_zones=mask_exclusion_zones
